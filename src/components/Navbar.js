5b49929d06cf7cfd75a25230118e8688316c4940
Zena Bos <zta.bos@gmail.com>
2022-03-03 13:10:16 +0100

    add logoutbutton

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
 import { NavLink } from "react-router-dom";
-
+import { AuthContext } from "../context/auth.context";
 
 export default function NavBar() {
+  const { logOutUser } = useContext(AuthContext);
+
   return (
     <div>
       <NavLink to="/">Home</NavLink>
       <NavLink to="/meetings">My meetings</NavLink> |
       <NavLink to="/meetings/create">New Meeting</NavLink>
+      <button onClick={logOutUser}>Logout</button>
     </div>
   );
 }
