b4c421d102eb81632352b1a95ff07df208cca479
Zena Bos <zta.bos@gmail.com>
2022-03-03 13:11:07 +0100

    conditional rendering homepage

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../context/auth.context";
 
 export default function Homepage() {
+  const { isLoggedIn, user } = useContext(AuthContext);
+  console.log(user)
+
   return (
-    <div>Homepage</div>
-  )
+    <div>
+      {user && (
+        <div>
+          <p>Hi {user.name} !</p>
+        </div>
+      )}
+
+      {!user && (
+        <div>
+          <h1>Meetive </h1>
+          <h2>meet effective</h2>
+            <Link to="/signup">
+              <button>Sign Up</button>
+            </Link>
+            <Link to="/login">
+              <button>Login</button>
+            </Link>
+        </div>
+      )}
+    </div>
+  );
 }
