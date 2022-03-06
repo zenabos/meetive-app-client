fe69496ca13ea79cfec92419ac8e5df331b6a194
Zena Bos <zta.bos@gmail.com>
2022-03-06 11:24:01 +0100

    cleanup code

--- src/components/CreateTopic.js
+++ src/components/CreateTopic.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useContext, useState } from "react";
-import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { AuthContext } from "../context/auth.context";
 
